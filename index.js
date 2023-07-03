f1257e554dfc0e2eccb80fde40ace1a9f9760a87
olupprince <99639095+olupprince@users.noreply.github.com>
2023-07-03 08:09:38 +0100

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const joi = require("joi");
 const { MongoClient } = require("mongodb");
 const app = express();
 const bodyParser = require("body-parser");
-const uri = "mongodb+srv://prince:Adawales01@prince.cd1a4as.mongodb.net/";
+const PORT = process.env.PORT || 3000;
+const uri = "mongodb://127.0.0.1:27017/sign-up";
 
-mongoose.connect = (uri,
-{
+MongoClient.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -16,24 +17,69 @@ mongoose.connect = (uri,
   .catch((error) => {
     console.error("error connecting to Mongo:", error);
   });
-console.log("connection great", client);
 
 app.use(bodyParser.json());
 
-app.post("/createaccount", (req, res) => {
-  res.send("heyyyyy");
+const yep = mongoose.Schema({
+  Email: String,
+  Password: String,
+  username: String,
 });
 
-app.get("/balance", (req, res) => {
-  res.send("Balance");
+const signUp = mongoose.model("user-login", yep);
+// create five diff. endpoints, first is to sign up, second is login, third is to create a product, fourth is to get all product and last get single product
+
+const schema = joi.object({
+  Email: joi.string().email().lowercase().required(),
+  Password: joi.string().min(6).required(),
+  repeat_password: joi.ref("Password"),
+  username: joi.string().alphanum().min(3).max(10).required(),
+  access_token: [joi.string(), joi.number()],
 });
 
-app.get("/balance/:accountNumber", (req, res) => {
-  res.send("account");
+// sign up
+app.post("/signup", (req, res) => {
+  const { error, value } = schema.validate(req.body);
+  console.log(req.body);
+  const newUser = new signUp({
+    Email: value.Email,
+    Password: value.Password,
+    username: value.username,
+  });
+  newUser.save();
+  res.status(201).send(newUser);
 });
 
-app.post("/transfer", (req, res) => {
-  res.send("transfer");
+// login
+app.post("/login", (req, res) => {
+  const { email, passwor } = schema.validate(req.body);
+});
+// product
+const productSchema = mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
 });
 
-app.listen(3000, console.log("Server is running "));
+const Product = mongoose.model("Product", productSchema);
+
+app.post("/products", (req, res) => {
+  const { name, price, description } = req.body;
+  const newProduct = new Product({
+    name,
+    price,
+    description,
+  });
+  newProduct.save();
+  res.status(201).send(newProduct);
+});
+
+// get
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+app.listen(3001, () => {
+  console.log("server is listening on port");
+});
